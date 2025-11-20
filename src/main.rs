517677b7f2b5fd0f92f8f949f14c08c02abf8049
Juan Mireles <jmireles@torrentialdata.com>
2025-11-20 10:34:12 -0500

    finish project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
@@ -17,27 +18,36 @@ fn main() {
         "encrypt" => {
             println!("Starting encryption for file: {}", file_path);
 
-            let file_contents: Vec<u8> = fs::read(file_path)
-                .expect("Failed to read the file. Check if the path is correct!");
+            let output_path = format!("{}.enc", file_path);
 
-            let key_bytes: &[u8] = key.as_bytes();
-            let new_data: Vec<u8> = file_contents
-                .iter()
-                .zip(key_bytes.iter().cycle())
-                .map(|(data_byte, key_byte)| *data_byte ^ *key_byte)
-                .collect();
+            process_file(file_path, &output_path, key)
+                .expect("A critical file I/O error occurred during processing.");
 
-            let output_file_path: String = format!("{}.enc", file_path);
-
-            fs::write(&output_file_path, new_data).expect("Failed to write the encrypted file!");
-
-            println!("Success! File encrypted to: {}", output_file_path);
+            println!("Success! File processed.");
         }
         "decrypt" => {
-            println!("Starting decryption for file: {}", file_path);
+            let output_path: &str = file_path.trim_end_matches(".enc");
+
+            process_file(file_path, output_path, key)
+                .expect("A critical file I/O error occurred during processing.");
         }
         _ => {
             println!("Error: Action must be 'encrypt' or 'decrypt'.");
         }
     }
+}
+
+fn process_file(file_path: &str, output_path: &str, key: &str) -> io::Result<()> {
+    let file_contents: Vec<u8> = fs::read(file_path)?;
+
+    let key_bytes: &[u8] = key.as_bytes();
+    let new_data: Vec<u8> = file_contents
+        .iter()
+        .zip(key_bytes.iter().cycle())
+        .map(|(data_byte, key_byte)| *data_byte ^ *key_byte)
+        .collect();
+
+    fs::write(output_path, new_data)?;
+
+    Ok(())
 }
